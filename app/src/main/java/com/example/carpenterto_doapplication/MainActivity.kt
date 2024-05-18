8b7c0dac82a3ecac4be5a291c3c69ec8846a04d6
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-05-18 16:13:31 +0800

    added super a lot

--- app/src/main/java/com/example/carpenterto_doapplication/MainActivity.kt
+++ app/src/main/java/com/example/carpenterto_doapplication/MainActivity.kt
@@ -1,20 +1,73 @@
 package com.example.carpenterto_doapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.carpenterto_doapplication.dashboard.ReportsFragment
+import com.example.carpenterto_doapplication.dashboard.TasksFragment
+import com.example.carpenterto_doapplication.databinding.ActivityMainBinding
+import com.example.carpenterto_doapplication.util.UiUtil
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+import java.text.DateFormat
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindHeader()
+
+        binding.settingsButton.setOnClickListener {
+            UiUtil.showToast(applicationContext, "clicked settings")
         }
+
+        binding.bottomNavBar.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_menu_tasks -> {
+                    replaceFragment(TasksFragment())
+                    UiUtil.showToast(applicationContext, "clicked tasks")
+                }
+                R.id.bottom_menu_reports -> {
+                    replaceFragment(ReportsFragment())
+                    UiUtil.showToast(applicationContext, "clicked reports")
+                }
+
+            }
+            false
+        }
+
+    }
+
+    private fun bindHeader() {
+        val calendar = Calendar.getInstance().time
+        val dateFormat = DateFormat.getDateInstance().format(calendar)
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+        Firebase.firestore
+            .collection("users")
+            .whereEqualTo("id", userId)
+            .get()
+            .addOnSuccessListener {
+                binding.firstNameText.text = it.documents[0].data?.get("fullName").toString().substringBefore(" ")
+            }
+            .addOnFailureListener {
+                UiUtil.showToast(applicationContext, it.localizedMessage?: "Something went wrong")
+            }
+
+        binding.dateTodayText.text = "Today: " + dateFormat
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
