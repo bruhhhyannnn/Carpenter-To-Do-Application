d6fadc772de44b25592670a8668022333df6a73d
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-05-18 11:00:18 +0800

    finished splash welcome where it updates name

--- app/src/main/java/com/example/carpenterto_doapplication/splash_art/SplashWelcomeActivity.kt
+++ app/src/main/java/com/example/carpenterto_doapplication/splash_art/SplashWelcomeActivity.kt
@@ -3,22 +3,43 @@ package com.example.carpenterto_doapplication.splash_art
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.carpenterto_doapplication.MainActivity
-import com.example.carpenterto_doapplication.R
-import com.example.carpenterto_doapplication.authentication.RegistrationActivity
+import com.example.carpenterto_doapplication.databinding.ActivitySplashWelcomeBinding
+import com.example.carpenterto_doapplication.util.UiUtil
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class SplashWelcomeActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivitySplashWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_welcome)
+        binding = ActivitySplashWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getName()
+    }
+
+    fun getName() {
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+        Firebase.firestore
+            .collection("users")
+            .whereEqualTo("id", userId)
+            .get()
+            .addOnSuccessListener {
+                binding.nameText.text = it.documents[0].data?.get("username").toString()
+            }
+            .addOnFailureListener {
+                UiUtil.showToast(applicationContext, it.localizedMessage?: "Something went wrong")
+            }
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2500)
+        }, 4000)
     }
 }
