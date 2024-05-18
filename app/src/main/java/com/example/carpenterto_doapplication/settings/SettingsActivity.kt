76c71488e2a17ca266adb664b2f5823b3691964b
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-05-18 22:34:22 +0800

    added logout and about functionality, but the i ignored the UI already and sticked to a more conventional one (no design)

--- app/src/main/java/com/example/carpenterto_doapplication/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/carpenterto_doapplication/settings/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.carpenterto_doapplication.settings
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carpenterto_doapplication.R
 import com.example.carpenterto_doapplication.dashboard.ReportsFragment
 import com.example.carpenterto_doapplication.dashboard.TasksFragment
 import com.example.carpenterto_doapplication.databinding.ActivitySettingsBinding
+import com.example.carpenterto_doapplication.splash_art.SplashActivity
 import com.example.carpenterto_doapplication.util.UiUtil
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -29,14 +32,46 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         binding.aboutButton.setOnClickListener {
-
+            showAboutAlertDialog()
         }
 
         binding.logoutButton.setOnClickListener {
-
+            showLogoutAlertDialog()
         }
 
 
+    }
+
+    private fun showAboutAlertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("About this application")
+        builder.setMessage("This is a system to ease out the mundane tasks that is done in day-to-day life of a very hard working person.")
+        builder.setPositiveButton("Okay!") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showLogoutAlertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Are you sure?")
+        builder.setMessage("Please make sure you have completed all your tasks.")
+        builder.setPositiveButton("Log Out") { dialog, _ ->
+            logout()
+        }
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this, SplashActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
     private fun bindHeader() {
