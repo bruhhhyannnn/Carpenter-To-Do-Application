8344fd63428697eb36f2129e1d032288ac7c7f78
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-05-20 22:50:17 +0800

    latest data model final

--- app/src/main/java/com/example/carpenterto_doapplication/data_model/TaskModel.kt
+++ app/src/main/java/com/example/carpenterto_doapplication/data_model/TaskModel.kt
@@ -1,8 +1,8 @@
 package com.example.carpenterto_doapplication.data_model
 
 data class TaskModel(
-    var reportId : Int,
-    var dateGenerated : String = "",
-    var timeGenerated : String = "",
-    var fullName : String = ""
+    var userId : String = "",
+    var machineId : Int,
+    var taskId : Int,
+    var taskName : String = "",
 )
