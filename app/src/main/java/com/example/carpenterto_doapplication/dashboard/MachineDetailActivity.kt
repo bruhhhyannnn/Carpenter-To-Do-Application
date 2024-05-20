fc6513c0d637c41fa15f1df61a74b32509cb456b
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-05-20 09:47:29 +0800

    current working

--- app/src/main/java/com/example/carpenterto_doapplication/dashboard/MachineDetailActivity.kt
+++ app/src/main/java/com/example/carpenterto_doapplication/dashboard/MachineDetailActivity.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carpenterto_doapplication.R
 import com.example.carpenterto_doapplication.adapter.ChecklistAdapter
+import com.example.carpenterto_doapplication.databinding.ActivityMachineDetailBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MachineDetailActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMachineDetailBinding
     private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_machine_detail)
+        binding = ActivityMachineDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val machineId = intent.getIntExtra("machineId", 0)
         val machineName = intent.getStringExtra("machineName") ?: "Unknown"
@@ -26,16 +30,31 @@ class MachineDetailActivity : AppCompatActivity() {
         val tasks = intent.getStringArrayListExtra("tasks") ?: arrayListOf()
         val tasksCompleted = intent.getBooleanArrayExtra("tasksCompleted") ?: BooleanArray(tasks.size)
 
-        val tvMachineName = findViewById<TextView>(R.id.machine_name)
-        tvMachineName.text = machineName
+        binding.machineName.text = machineName
 
         val recyclerView = findViewById<RecyclerView>(R.id.checklist_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = ChecklistAdapter(tasks, tasksCompleted, machineId)
 
-        val btnGenerateReport = findViewById<Button>(R.id.generate_report_button)
-        btnGenerateReport.setOnClickListener {
+        binding.generateReportButton.setOnClickListener {
             Toast.makeText(this, "Generate Report Clicked", Toast.LENGTH_SHORT).show()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Do you want to generate report?")
+            builder.setMessage("Please make sure you have completed all the tasks for this maintenance.")
+            builder.setPositiveButton("Generate") { dialog, _ ->
+                // this will generate the report by making a pdf file containing the checklist items that user have accomplished while
+                // incorporating it with the users profile i.e., full name, username and date of creation
+            }
+            builder.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+        binding.saveProgressButton.setOnClickListener {
+            // implement the save progress button functionality by saving the checked lists and storing it to firebase firestore for a particular user
+            //
         }
     }
 }
